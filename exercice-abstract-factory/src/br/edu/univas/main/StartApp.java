643e786b9036dfa99c5459383eaad619ea447a6a
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-08-05 21:59:40 -0300

    Correcao exercicio-abstract-factory

--- exercice-abstract-factory/src/br/edu/univas/main/StartApp.java
+++ exercice-abstract-factory/src/br/edu/univas/main/StartApp.java
@@ -1,27 +1,36 @@
 package br.edu.univas.main;
 
-import br.edu.univas.vo.MacButton;
-import br.edu.univas.vo.MacCheckBox;
-import br.edu.univas.vo.WindowsButton;
-import br.edu.univas.vo.WindowsCheckBox;
+import br.edu.univas.factory.AbstractFactory;
+import br.edu.univas.factory.FactoryProvider;
+import br.edu.univas.vo.Button;
+import br.edu.univas.vo.CheckBox;
 
 public class StartApp {
 
 	public static void main(String[] args) {
-		String osName = System.getProperty("os.name").toLowerCase();
-		if (osName.contains("windows")) {
-			WindowsButton button = new WindowsButton();
-			button.paint();
-			
-			WindowsCheckBox checkBox = new WindowsCheckBox();
-			checkBox.paint();
-			
-		} else if (osName.contains("mac")) {
-			MacButton button = new MacButton();
-			button.paint();
-			
-			MacCheckBox checkBox = new MacCheckBox();
-			checkBox.paint();
-		}
+		String osName = System.getProperty("os.name");
+		
+		AbstractFactory factory = FactoryProvider.getFactory(osName);
+		
+		Button button = factory.createButton();
+		button.paint();
+		
+		CheckBox checkBox = factory.createCheckBox();
+		checkBox.paint();
+		
+//		if (osName.contains("windows")) {
+//			WindowsButton button = new WindowsButton();
+//			button.paint();
+//			
+//			WindowsCheckBox checkBox = new WindowsCheckBox();
+//			checkBox.paint();
+//			
+//		} else if (osName.contains("mac")) {
+//			MacButton button = new MacButton();
+//			button.paint();
+//			
+//			MacCheckBox checkBox = new MacCheckBox();
+//			checkBox.paint();
+//		} 
 	}
 }
