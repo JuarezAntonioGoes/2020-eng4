059c4e732a343913323fcc68812f35598f31a013
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-08-20 21:56:23 -0300

    Fix exercise-composite

--- exercise-composite/src/br/edu/univas/main/StartApp.java
+++ exercise-composite/src/br/edu/univas/main/StartApp.java
@@ -1,19 +1,31 @@
 package br.edu.univas.main;
 
 import br.edu.univas.vo.Circle;
+import br.edu.univas.vo.Container;
 import br.edu.univas.vo.Rectangle;
 import br.edu.univas.vo.Square;
 
 public class StartApp {
 
 	public static void main(String[] args) {
+		Container container = new Container();
 		Circle c = new Circle();
-		c.draw();
+		container.addComponent(c);
 		
 		Square s = new Square();
-		s.draw();
+		container.addComponent(s);
 		
 		Rectangle r = new Rectangle();
-		r.draw();
+		container.addComponent(r);
+		
+		Container container2 = new Container();
+		container2.addComponent(new Rectangle());
+		
+		Container container3 = new Container();
+		container3.addComponent(new Circle());
+		container2.addComponent(container3);
+		container.addComponent(container2);
+		
+		container.draw();
 	}
 }
