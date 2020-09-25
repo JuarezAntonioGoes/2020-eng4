2bc7811cdfa2ba3ec843014a19ad6398e0a4cf74
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-09-24 21:59:26 -0300

    Fix exercise-builder

--- exercise-builder/src/br/edu/univas/main/App.java
+++ exercise-builder/src/br/edu/univas/main/App.java
@@ -5,9 +5,25 @@ import br.edu.univas.vo.Vehicle;
 public class App {
 
 	public static void main(String[] args) {
-		Vehicle vehicle1 = new Vehicle("FIAT", "Palio ELX", "Prata", 4, 2017, 2016);
+		//Vehicle vehicle1 = new Vehicle("FIAT", "Palio ELX", "Prata", 4, 2017, 2016);
+		Vehicle vehicle1 = new Vehicle.Builder()
+				.withModel("Palio ELX")
+				.withModelYear(2017)
+				.withBrand("FIAT")
+				.withColor("Prata")
+				.withManufactureYear(2016)
+				.withPort(4)
+				.build();
 		
-		Vehicle vehicle2 = new Vehicle("Volksvagen", "Gol", "Branco", 4, 2019, 2019);
+		//Vehicle vehicle2 = new Vehicle("Volksvagen", "Gol", "Branco", 4, 2019, 2019);
+		Vehicle vehicle2 = new Vehicle.Builder()
+				.withModel("Gol")
+				.withColor("Branco")
+				.withModelYear(2019)
+				.withBrand("Volksvagen")
+				.withPort(4)
+				.withManufactureYear(2019)
+				.build();
 		
 		System.out.println("Veículo 1:");
 		System.out.println(vehicle1);
--- exercise-builder/src/br/edu/univas/vo/Vehicle.java
+++ exercise-builder/src/br/edu/univas/vo/Vehicle.java
@@ -9,7 +9,7 @@ public class Vehicle {
 	private int modelYear;
 	private int manufactureYear;
 	
-	public Vehicle(String brand, String model, String color, int port, int modelYear, int manufactureYear) {
+	private Vehicle(String brand, String model, String color, int port, int modelYear, int manufactureYear) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -19,17 +19,55 @@ public class Vehicle {
 		this.manufactureYear = manufactureYear;
 	}
 	
+	public static class Builder {
+		private String brand;
+		private String model;
+		private String color;
+		private int port;
+		private int modelYear;
+		private int manufactureYear;
+		
+		public Builder withBrand(String brand) {
+			this.brand = brand;
+			return this;
+		}
+		
+		public Builder withModel(String model) {
+			this.model = model;
+			return this;
+		}
+		
+		public Builder withColor(String color) {
+			this.color = color;
+			return this;
+		}
+		
+		public Builder withPort(int port) {
+			this.port = port;
+			return this;
+		}
+		
+		public Builder withModelYear(int modelYear) {
+			this.modelYear = modelYear;
+			return this;
+		}
+		
+		public Builder withManufactureYear(int manufactureYear) {
+			this.manufactureYear = manufactureYear;
+			return this;
+		}
+		
+		public Vehicle build() {
+			return new Vehicle(brand, model, 
+					color, port, modelYear, manufactureYear);
+		}
+	}
+	
 	public String getBrand() {
 		return brand;
 	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
 	public String getModel() {
 		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
 	}
 	public String getColor() {
 		return color;
@@ -40,22 +78,13 @@ public class Vehicle {
 	public int getPort() {
 		return port;
 	}
-	public void setPort(int port) {
-		this.port = port;
-	}
 	public int getModelYear() {
 		return modelYear;
-	}
-	public void setModelYear(int modelYear) {
-		this.modelYear = modelYear;
 	}
 	public int getManufactureYear() {
 		return manufactureYear;
 	}
-	public void setManufactureYear(int manufactureYear) {
-		this.manufactureYear = manufactureYear;
-	}
-
+	
 	@Override
 	public String toString() {
 		return "Vehicle [brand=" + brand + ", model=" + model + ", color=" + color + ", port=" + port + ", modelYear="
